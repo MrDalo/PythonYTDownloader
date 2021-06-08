9f89c90a45f9006fde986cd2d0a4400c24f6ce7b
MrDalo <dadokralik@gmail.com>
2021-06-08 12:50:23 +0200

    Connect progress bar

--- downloader.py
+++ downloader.py
@@ -20,30 +20,29 @@ class Downloader(QtWidgets.QMainWindow, Ui_MainWindow):
         self.folderPath = None
         self.fileSize = 0
         self.yt = None
+        self.progressBar.setValue(0)
 
         self.buttonPath.clicked.connect(self.findPath)
         self.lineEditLink.textChanged.connect(self.signalFun)
 
         self.buttonDownload.clicked.connect(self.download)
 
-
-
     def findPath(self):
         self.folderPath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder')
         self.lineEditPath.setText(self.folderPath)
 
-        #TODO dopisanie cesty rucne
 
 
     def signalFun(self):
-        #TODO dorob namapovanie videa
-        #namapovanie videa a zsitenie jeho dostupnosti
+
         url = self.lineEditLink.text()
 
         try:
-            self.yt = YouTube(url, on_progress_callback=self.progressBar)
+            self.yt = YouTube(url)
+            self.yt.register_on_progress_callback(self.progress_bar)
             self.readyToDownload = True
         except:
+            self.labelError.setStyleSheet("color : red;")
             self.labelError.setText("Unvalid video URL")
             self.labelVideoTitle.setText('')
             self.readyToDownload = False
@@ -51,13 +50,18 @@ class Downloader(QtWidgets.QMainWindow, Ui_MainWindow):
 
         #pridanie fotky a titulu
         self.labelVideoTitle.setText(self.yt.title)
+        self.labelError.setStyleSheet("color : green;")
         self.labelError.setText("Video successfully found")
 
-    def progressBar(self, chunk, bytes_remaining):
-        print("I am in progressBar")
+    def progress_bar(self, stream, chunk, bytes_remaining):
+        size = stream.filesize
+        p = ((float(size) - float(bytes_remaining)) / float(size)) * float(100)
+        print(str(p) + '%', bytes_remaining, ' ', size)
+        self.progressBar.setValue(p)
 
     def download(self):
         if self.lineEditLink.text() and self.lineEditPath.text() and self.readyToDownload:
+            self.labelError.setStyleSheet("color : blue;")
             self.labelError.setText("Starting download")
             # selecting right video
             video = self.yt.streams
@@ -70,44 +74,10 @@ class Downloader(QtWidgets.QMainWindow, Ui_MainWindow):
                 video = all_video.filter(progressive=True)
                 video = video.first()
             # downloading the video
-            self.labelError.setText(f"{self.labelError.text()} \n {video}")
+            self.labelError.setStyleSheet("color : blue;")
+            self.labelError.setText(f"{self.labelError.text()}\n\n{video}")
             video.download(self.folderPath)
+            self.labelError.setStyleSheet("color : green;")
+            self.labelError.setText("Video is successfully download")
         else:
             self.labelError.setText("Error occurs -> Video Link or  Folder Path are not filled correctly")
-
-
-def main():
-    # input of the URl form the user
-    url = input("Add url of the YT video: ")
-
-    try:
-        yt = YouTube(url)
-    except:
-        print("Unvalid video URL")
-        return None
-
-    # creating 'Downloads' file
-    path = os.getcwd()
-    print(path)
-    path = os.path.join(path, 'Downloads')
-    os.makedirs(path, exist_ok=True)
-
-    video = yt.streams
-    all_video = video
-    video = video.filter(progressive=True, res='720p')
-
-    # choosing first option
-    video = video.first()
-    if video is None:
-        video = all_video.filter(progressive=True)
-        video = video.first()
-
-
-
-    # download video
-    print(f'Final video: {video}')
-    video.download(path)
-
-
-if __name__ == "__main__":
-    main()
